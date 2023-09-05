ba2c957450abdc9091b529d0ebed2ac015a4dfdc
Deniz <dnz66@hotmail.com>
2023-09-05 11:52:19 -0400

    US_04 is Done

---
+++ src/O2_04.java
@@ -0,0 +1,29 @@
+import Utilities.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class O2_04 extends BaseDriver {
+
+    @Test
+    public void passwordNotMatchTest(){
+        adminLoginNavigateToAddUser();
+
+
+
+        driver.findElement(By.xpath("(//input[@type='password'])[1]")).sendKeys("Test123!");
+        driver.findElement(By.xpath("(//input[@type='password'])[2]")).sendKeys("Test444!");
+        String message = "Passwords do not match";
+
+        WebElement errorMessage = driver.findElement(By.xpath("//span[text()='Passwords do not match']"));
+
+        Assert.assertEquals("rgba(235, 9, 16, 1)", errorMessage.getCssValue("color"));
+        Assert.assertEquals(message, errorMessage.getText());
+
+        waitAndQuit();
+
+    }
+
+}
